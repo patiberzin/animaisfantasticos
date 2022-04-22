d8df57d093c049d4558b63c5eb10d5482849e2e2
patiberzin <pberzincand@gmail.com>
2022-04-22 18:10:04 -0300

    anima scroll

--- animacao-ao-scroll/js/script.js
+++ animacao-ao-scroll/js/script.js
@@ -84,12 +84,30 @@ initSrollSuave();
 
 /////////////////////
 
-const sections = document.querySelectorAll('.js-scroll'); //1º
+function initAnimaScroll() {
 
-function animaScroll() { //2º
-    console.log('sim');
+    const sections = document.querySelectorAll('.js-scroll'); //1º
+
+    if (sections.length) {
+        const windowMetade = window.innerHeight * 0.6;
+
+        function animaScroll() { //2º
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if (isSectionVisible) {
+                    section.classList.add('ativo');
+                } else {
+                    section.classList.remove('ativo');
+                }
+            });
+        }
+
+
+        animaScroll();
+
+        window.addEventListener('scroll', animaScroll);
+    }
 }
 
-sections.forEach((link) => {
-    link.addEventListener('scroll', animaScroll);
-});
+initAnimaScroll();
